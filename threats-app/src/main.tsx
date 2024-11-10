8198e9e2d85b866e3caf554e6865d994b6e8146b
lexcf <lexcf@bk.ru>
2024-11-10 15:22:03 +0300

    pwa

--- threats-app/src/main.tsx
+++ threats-app/src/main.tsx
@@ -30,6 +30,15 @@ const router = createBrowserRouter([
   }
 ], { basename: '/rip_frontend' });
 
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/rip_frontend/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
+  })
+}
 // Рендерим приложение с провайдером роутера
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
