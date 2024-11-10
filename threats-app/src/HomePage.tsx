74cdf0ffee62fdfffe0ac78e97daff3b9aab4a0a
lexcf <lexcf@bk.ru>
2024-11-10 15:25:56 +0300

    fix header url

--- threats-app/src/HomePage.tsx
+++ threats-app/src/HomePage.tsx
@@ -9,7 +9,7 @@ export default function HomePage() {
     <div className="container-fluid bg-dark text-light min-vh-100 d-flex flex-column">
       {/* Шапка */}
       <header className="d-flex justify-content-between align-items-center px-5 py-3" style={{ backgroundColor: '#333', height: '70%', maxHeight: '60px', width: '1990px', marginLeft: '-30px' }}>
-        <a href="/" className="text-light fs-4">Мониторинг угроз</a>
+        <a href="/rip_frontend" className="text-light fs-4">Мониторинг угроз</a>
         <Navbar />
       </header>
 
